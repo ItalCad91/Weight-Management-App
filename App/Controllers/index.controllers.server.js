d13abb9341dd5d3981a2ec13f34c691012aa047f
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-10-28 16:42:17 -0400

    update

--- App/Controllers/index.controllers.server.js
+++ App/Controllers/index.controllers.server.js
@@ -14,16 +14,12 @@ export function displayContactPage(req, res, next)
     res.render('index', {title : 'DBMS Desktop App - Contact Us', page:'contactus'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
 };
 
-export function displayDownloadPage(req, res, next) 
+export function displayThankYouPage(req, res, next) 
 {
-<<<<<<< HEAD
-    res.render('index', {title : 'Weight Management App Login', page:'login'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
+    res.render('index', {title : 'Weight Management App Login', page:'ThankYou'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
 };
 
 export function displayDownloadPage(req, res, next) 
 {
-    res.render('index', {title : 'Weight Management App Download', page:'download'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
-=======
-    res.render('index', {title : 'DBMS Desktop App - Download', page:'download'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
->>>>>>> b7f64198274cdd0369cf9a0715acd7686fb9c32d
+    res.render('index', {title : 'Weight Management App Download', page:'download'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX   
 };
--- App/Routes/index.routes.server.js
+++ App/Routes/index.routes.server.js
@@ -1,24 +1,17 @@
-<<<<<<< HEAD
-import {displayHomePage, displayCaloriesPage, displayRemainingPage, displayLoginPage, displayDownloadPage} from '../Controllers/index.controllers.server.js'
-=======
-import {displayHomePage, displayAboutPage, displayContactPage, displayDownloadPage} from '../Controllers/index.controllers.server.js'
->>>>>>> b7f64198274cdd0369cf9a0715acd7686fb9c32d
+import {displayHomePage, displayAboutPage,displayContactPage, displayThankYouPage, displayDownloadPage} from '../Controllers/index.controllers.server.js'
+
 import { Router } from "express";
 
 
 const router = Router(); // I am initializing the router function.
 
 // app.use('/')
-router.get('/', displayHomePage);
-<<<<<<< HEAD
-router.get('/calories', displayCaloriesPage);
-router.get('/remaining', displayRemainingPage);
-router.get('/login', displayLoginPage);
-=======
-router.get('/contactus', displayContactPage);
+
 router.get('/about', displayAboutPage);
->>>>>>> b7f64198274cdd0369cf9a0715acd7686fb9c32d
+router.get('/contactus', displayContactPage);
 router.get('/download', displayDownloadPage);
+router.get('/', displayHomePage);
+router.get('/login', displayThankYouPage);
 
 
 
